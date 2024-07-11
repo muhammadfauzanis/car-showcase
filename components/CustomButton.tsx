d58ce868faf6d42e0f371286ea390c3584c5c1cc
mfauzann982 <mfauzann982@gmail.com>
2024-07-11 20:37:35 +0700

    add hero page

---
+++ components/CustomButton.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { MouseEventHandler } from 'react';
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
